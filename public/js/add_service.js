0cc994b8461032b58ddcfbc0d6697a3a5d9ff4bb
Lori Seo <lorijseo@gmail.com>
2024-03-17 22:19:00 -0700

    comments for hbs and add pages

--- public/js/add_service.js
+++ public/js/add_service.js
@@ -3,46 +3,42 @@
 // Based on: CS 340 starter code 'Step 5 Adding New Data'
 // Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
-// Get the objects we need to modify
+
 let addServiceForm = document.getElementById('add-service-form-ajax');
 
-// Modify the objects we need
+// Execute this when we submit the current form
 addServiceForm.addEventListener("submit", function (e) {
 
-    // Prevent the form from submitting
+    // Prevent the form from submitting automatically
     e.preventDefault();
 
-    // Get form fields we need to get data from
+    //Get the values from form fields and save it into an object
     let inputServiceName = document.getElementById("input-service-name");
     let inputServiceCost = document.getElementById("input-service-cost");
     let inputServiceDescription = document.getElementById("input-service-description");
 
-    // Get the values from the form fields
     let ServiceNameValue = inputServiceName.value;
     let ServiceCostValue = inputServiceCost.value;
     let ServiceDescriptionValue = inputServiceDescription.value;
 
-
-    // Put our data we want to send in a javascript object
     let data = {
         service_name: ServiceNameValue,
         service_cost: ServiceCostValue,
         service_description: ServiceDescriptionValue
     }
 
-    // Setup our AJAX request
+    //Initialize AJAX request to receive data
+    // Upon receiving data, we add row to table, clear input fields, and send data
+    // Handles errors if AJAX not set up correctly or invalid inputs
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "/add-service-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-            // Add the new data to the table
             addRowToTable(xhttp.response);
 
-            // Clear the input fields for another transaction
             inputServiceName.value = '';
             inputServiceCost.value = '';
             inputServiceDescription.value = '';
@@ -52,27 +48,19 @@ addServiceForm.addEventListener("submit", function (e) {
         }
     }
 
-    // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 
 })
 
 
-// Creates a single row from an Object representing a single record from 
-// bsg_people
+// Creates a single row when receiving an object as an argument 
 addRowToTable = (data) => {
 
-    // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("services-table");
-
-    // Get the location where we should insert the new row (end of table)
-    let newRowIndex = currentTable.rows.length;
-
-    // Get a reference to the new row from the database query (last object)
+    // Find new data from query
     let parsedData = JSON.parse(data);
     let newRow = parsedData[parsedData.length - 1]
 
-    // Create a row and 4 cells
+    // Create containers for new row, cells, and delete button, then fill them with new data values
     let row = document.createElement("TR");
     let serviceIdCell = document.createElement("TD");
     let serviceNameCell = document.createElement("TD");
@@ -88,6 +76,7 @@ addRowToTable = (data) => {
     serviceCostCell.innerText = newRow.service_cost;
     serviceDescriptionCell.innerText = newRow.service_description;
 
+    //include on click delete functionality for corresponding row of data
     deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
     deleteBtn.onclick = function () {
@@ -102,20 +91,12 @@ addRowToTable = (data) => {
     row.appendChild(serviceDescriptionCell);
     row.appendChild(deleteCell);
 
-    // Add a custom row attribute so the deleteRow function can find a newly added row
+    // initialize unique row attribute to easily identify row
     row.setAttribute('data-value', newRow.service_id);
 
+    // Add the row to the end of the table
     let currentTableBody = document.getElementById("services-table-body");
-    // Add the row to the table
     currentTableBody.appendChild(row);
 
-    // Find drop down menu, create a new option, fill data in the option (full name, id),
-    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
-
-    // let selectMenu = document.getElementById("select-visit-id");
-    // let option = document.createElement("option");
-    // option.text = newRow.pet_id + ' ' + newRow.pet_name;
-    // option.value = newRow.pet_id;
-    // selectMenu.add(option);
 }
 
