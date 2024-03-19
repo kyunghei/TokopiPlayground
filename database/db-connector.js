632ddb3eb8495d387a2064df7955d52ad7c79fce
Kyung Hei Seo <seoky@oregonstate.edu>
2024-03-18 18:40:06 -0700

    Removed extra lines in queries which caused error

--- database/db-connector.js
+++ database/db-connector.js
@@ -5,11 +5,11 @@ var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_seolo',
-    password        : '5157',
-    database        : 'cs340_seolo'
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_seoky',
+    password: '5501',
+    database: 'cs340_seoky'
 })
 
 // Export it for use in our applicaiton
